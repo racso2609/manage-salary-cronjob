0dbaa37b82d910abb219f25ce7a8d390c23132db
racso2609 <oscaremiliolugo@gmail.com>
2023-05-25 19:03:53 -0400

    fic

--- src/binance/expenses.ts
+++ src/binance/expenses.ts
@@ -2,4 +2,4 @@ import { createExpense } from 'src/utils/binance';
 
 const url = 'sapi/v1/c2c/orderMatch/listUserOrderHistory';
 const query = `timestamp=${Date.now()}&tradeType=SELL`;
-createExpense(url, query).then()
+createExpense(url, query).then();
