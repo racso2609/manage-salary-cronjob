1384cb8748e7d4d7095fae47931973c2f3a5974d
racso2609 <oscaremiliolugo@gmail.com>
2023-05-25 18:55:24 -0400

    remove dist from git

--- dist/binance/utils/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateSignature = void 0;
-const crypto = require("crypto");
-const generateSignature = (query, secret) => {
-    return crypto.createHmac("sha256", secret).update(query).digest("hex");
-};
-exports.generateSignature = generateSignature;
-//# sourceMappingURL=index.js.map
