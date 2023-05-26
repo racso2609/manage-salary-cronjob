7c04a747153c25da7c46b84b6701c101066ff597
racso2609 <oscaremiliolugo@gmail.com>
2023-05-25 20:14:27 -0400

    remove no needed config

--- pages/api/cron/expense.ts
+++ pages/api/cron/expense.ts
@@ -1,16 +1,7 @@
-import {  NextResponse } from "next/server";
 import { createExpense } from "utils/binance";
-
-export const config = {
-  runtime: "edge",
-};
 
 export default async function handler() {
   const url = "sapi/v1/c2c/orderMatch/listUserOrderHistory";
   const query = `timestamp=${Date.now()}&tradeType=SELL`;
   await createExpense(url, query);
-
-  return new NextResponse(null, {
-    status: 200,
-  });
 }
